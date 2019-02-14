64b10c854abbe68c5446f22e26cb337732ddef9b
akrherz <akrherz@iastate.edu>
2019-02-14 09:15:23 -0600

    reorg the JS vars and ordering

--- src/js/start.js
+++ src/js/start.js
@@ -1,16 +1,4 @@
 // Our bootstrapping
-var tilecache = "https://mesonet.agron.iastate.edu";
-var BACKEND = (window.location.host.indexOf(".local") > 0) ? "http://depbackend.local" : "https://mesonet-dep.agron.iastate.edu";
-var appstate = {
-	lastdate: null,
-	lat: null,
-	lon: null,
-	date: null,
-	date2: null,
-    metric: 0,
-	ltype: 'qc_precip'
-};
-
 $(document).ready(function(){
     build();
 });
