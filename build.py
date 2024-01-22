73ecc25a62911d66868228a21cb5b59f523d6374
akrherz <akrherz@iastate.edu>
2024-01-22 08:42:47 -0600

    mnt: format code with ruff

--- build.py
+++ build.py
@@ -1,18 +1,14 @@
 """Build this into a release for depmap-releases.
 
 """
-import os
-import sys
 import datetime
+import os
 import subprocess
+import sys
 
 RELPATH = "/home/akrherz/projects/depmap-releases"
 RELEASE = f"v{datetime.datetime.now():%Y%m%dT%H%M}"
-JSINC = [
-    'js/appvars.js',
-    'js/nextgen.js',
-    'js/start.js'
-]
+JSINC = ["js/appvars.js", "js/nextgen.js", "js/start.js"]
 
 
 def buildjs():
@@ -26,7 +22,7 @@ def buildjs():
         cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     (stdout, stderr) = proc.communicate()
-    if stdout != b'' or stderr != b'':
+    if stdout != b"" or stderr != b"":
         print(f"cmd: {cmd}")
         print(f"stdout: {stdout}")
         print(f"stderr: {stderr}")
@@ -36,9 +32,9 @@ def buildjs():
 
 def indexhtml():
     """index.html logic."""
-    with open(f"{RELPATH}/index.html", 'w', encoding="utf-8") as fh:
+    with open(f"{RELPATH}/index.html", "w", encoding="utf-8") as fh:
         donework = False
-        with open("src/index.html", 'r', encoding="utf-8") as ifh:
+        with open("src/index.html", "r", encoding="utf-8") as ifh:
             for line in ifh:
                 if line.find("WEB_INTERFACE_VERSION") > -1:
                     fh.write(line.replace("WEB_INTERFACE_VERSION", RELEASE))
@@ -46,7 +42,7 @@ def indexhtml():
                 if line.find("<!-- replaceme -->") == -1:
                     fh.write(line)
                 elif not donework:
-                    fh.write(f"<script src=\"{RELEASE}.js\"></script>\n")
+                    fh.write(f'<script src="{RELEASE}.js"></script>\n')
                     donework = True
 
 
@@ -60,13 +56,12 @@ def main():
     indexhtml()
     # Copy files
     subprocess.call(
-        f"rsync -av --delete src/css src/images src/lib {RELPATH}",
-        shell=True)
+        f"rsync -av --delete src/css src/images src/lib {RELPATH}", shell=True
+    )
     # git commit, tag, and push to github
     os.chdir(RELPATH)
     subprocess.call("git add *", shell=True)
-    subprocess.call(
-        f"git commit -m \"depmap release {RELEASE}\"", shell=True)
+    subprocess.call(f'git commit -m "depmap release {RELEASE}"', shell=True)
     subprocess.call(f"git tag {RELEASE}", shell=True)
     # Open browser to see what we have
     subprocess.call("gio open http://depmap-releases.local/", shell=True)
@@ -76,5 +71,5 @@ def main():
         print("ABORT git push.")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
