1532b65d2ea4621bff0513f4bb3a630973f82443
akrherz <akrherz@iastate.edu>
2025-08-15 10:56:44 -0500

    🐛 Fix layer toggles
    closes #92

--- tests/setup.js
+++ tests/setup.js
@@ -3,3 +3,13 @@ import { vi } from 'vitest';
 
 // Mock fetch API
 global.fetch = vi.fn();
+
+// Polyfill ResizeObserver required by OpenLayers in jsdom
+if (typeof global.ResizeObserver === 'undefined') {
+	global.ResizeObserver = class {
+		constructor(callback) { this.callback = callback; }
+		observe() { /* noop */ }
+		unobserve() { /* noop */ }
+		disconnect() { /* noop */ }
+	};
+}
